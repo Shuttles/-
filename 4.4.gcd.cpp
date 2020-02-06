eff50fb0685631d392233316fb31c67b1c90f57f
chenzheyu <2287441467@qq.com>
2020-02-06 14:14:22 +0800

    some changes

--- 4.4.gcd.cpp
+++ 4.4.gcd.cpp
@@ -12,9 +12,10 @@ int gcd(int a, int b) {
     if (b == 0) return a;//这是递归程序的边界条件
     return gcd(b, a % b);
 }
+
 /*
 int gcd(int a, int b) {
-    return (b ? gcd(b, b % a) : a);
+    return (b ? gcd(b, a % b) : a);
 }
 */
 
--- 4.my_printf.c
+++ 4.my_printf.c
@@ -2,109 +2,111 @@
 	> File Name: 4.my_printf.c
 	> Author: 
 	> Mail: 
-	> Created Time: 2019年07月30日 星期二 19时32分21秒
+	> Created Time: 2019年07月31日 星期三 13时26分47秒
  ************************************************************************/
-
+//注意why const char???
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdarg.h>
 #include <inttypes.h>
 
-int reverse_num(int num, int *temp);
 
-int output_num_with_digit(int num, int digit);
+int reverse_num(int *x) {
+    int cnt = 0, ans = 0;
+    int temp = *x;
+    do {
+        ans = ans * 10 + temp % 10;
+        temp /= 10;
+        cnt++;
+    } while (temp);
+    *x = ans;
+    return cnt;
+}
+
+int output_num(int num, int digit) {
+    int cnt = 0;
+    while (digit--) {
+        putchar(num % 10 + '0');
+        cnt++;
+        num /= 10;
+    }
+    return cnt;
+}
+
 
 int my_printf(const char *frm, ...) {
-    va_list arg;
-    va_start(arg, frm);
-    int i;
     int cnt = 0;
     #define PUTC(a) putchar(a), ++cnt
-    for (i = 0; frm[i]; i++) {
+    va_list arg;
+    va_start(arg, frm);
+    for (int i = 0; frm[i]; i++) {
         switch (frm[i]) {
             case '%': {
                 switch (frm[++i]) {
-                    case '%': PUTC(frm[i]);break;
+                    case '%': PUTC(frm[i]); break;
                     case 'd': {
-                        int xx = va_arg(arg, int), temp = 0, digit = 0;
+                        int xx = va_arg(arg, int);
                         uint32_t x;
                         if (xx < 0) {
                             PUTC('-');
                             x = -xx;
                         } else x = xx;
-                        int val1 = x / 100000, val2 = x % 100000;
-                        int temp1 = 0, temp2 = 0;
-                        int digit1 = reverse_num(val1, &temp1), digit2 = reverse_num(val2, &temp2);
-                        if (val1) digit2 = 5;
+                        int num1 = x / 100000, num2 = x % 100000;
+                        int digit1 = reverse_num(&num1), digit2 = reverse_num(&num2);
+                        //printf("%d's digit1 = %d  ", num1, digit1);
+                        //printf("%d's digit2 = %d  ", num2, digit2);
+                        //思考一下为什么一定要加上下面两行？
+                        //如果没加下面两行，输出的数之前总会多出个0?????
+                        if (num1) digit2 = 5;
                         else digit1 = 0;
-                        cnt += output_num_with_digit(temp1,digit1);
-                        cnt += output_num_with_digit(temp2,digit2);
-
+                        output_num(num1, digit1);
+                        output_num(num2, digit2);
+                        cnt += (digit1 + digit2);
                     } break;
                     case 's': {
                         const char *str = va_arg(arg, const char *);
-                        int j;
-                        for (j = 0; str[j]; j++) {
+                        for (int j = 0; str[j]; j++) {
                             PUTC(str[j]);
-                            cnt++;
                         }
-                        
                     } break;
                 }
-                
-    
-
             } break;
-        
-            default : PUTC(frm[i]);break;
-        }   
+            default: PUTC(frm[i]);
+        }
     }
-    return cnt;
+    va_end(arg);
     #undef PUTC
+    return cnt;
 }
+
+
 
 int main() {
-    int a = 123;
+    int a = 641;
     printf("hello world\n");
-    my_printf("hello world\n");
-    printf("int a = %d\n", a);
-    my_printf("int a = %d\n",a);
-    printf("int(1000) = %d\n", 1000);
-    my_printf("int(1000) = %d\n",1000);
-    printf("int(0) = %d\n", 0);
-    my_printf("int(0) = %d\n", 0);
+    my_printf("hello world\n");    
+    printf("%s\n", "suiyuejinhao");
+    my_printf("%s\n", "suiyuejinhao");
+    printf("int(a) = %d\n", a);
+    my_printf("int(a) = %d\n", a);
+    printf("int(123) = %d\n", 123);
+    my_printf("int(123) = %d\n", 123); 
     printf("int(-123) = %d\n", -123);
     my_printf("int(-123) = %d\n", -123);
-    printf("INT32_MAX = %d\n",INT32_MAX);
-    my_printf("INT32_MAX = %d\n",INT32_MAX);
+    printf("int(1000) = %d\n", 1000);
+    my_printf("int(1000) = %d\n", 1000);
+    printf("int(0) = %d\n", 0);
+    my_printf("int(0) = %d\n", 0);
+    printf("INT32_MAX = %d\n", INT32_MAX);
+    my_printf("INT32_MAX = %d\n", INT32_MAX);
     printf("INT32_MIN = %d\n", INT32_MIN);
     my_printf("INT32_MIN = %d\n", INT32_MIN);
-    char str[100] = "I love money";
-    my_printf("%s\n", str);
-    int n;
-    scanf("%d", &n);
-    my_printf(" has %d digits\n",my_printf("%d", n));
-    return 0;
 
-}
-
-int reverse_num(int num, int *temp) {
-    int digit = 0;
-    *temp = 0;
-    do {
-        *temp = *temp * 10 + num % 10;
-        num /= 10;
-        digit++;
-    } while (num);
-    return digit;
-}
-
-int output_num_with_digit(int num, int digit) {
-    int ret = 0;
-    while (digit--) {
-        putchar(num % 10 + 48);
-        num /= 10;
-        ret++;
+    int n = 0;
+    while (~scanf("%d", &n)) {
+        printf(" has %d digits\n", printf("%d", n));
+        my_printf(" has %d digits\n", my_printf("%d", n));
     }
-    return ret;
+    return 0;
 }
+
