c96bc43ec2ad320055fc035a241a5956ce670f42
chenzheyu <2287441467@qq.com>
2021-01-08 16:52:22 +0800

    输出彩色提示符以及EXPECT输出 泛型宏存在bug

--- project/haizei/test.cc
+++ project/haizei/test.cc
@@ -1,16 +1,31 @@
 #include <haizei/test.h>
 #include <string.h>
 #include <stdio.h>
+#include <math.h>
 
 int func_cnt = 0;
 Function func_arr[100];
+struct FunctionInfo haizei_test_info;//在源文件中定义，头文件中声明
 
 
 int RUN_ALL_TESTS() {
     for (int i = 0; i < func_cnt; i++) {
-        printf("Run Tests : %s\n", func_arr[i].str);
-        func_arr[i].func();
-        printf("Run End\n");
+        printf(GREEN("[====RUN====]") RED_HL(" %s\n"), func_arr[i].str);
+        haizei_test_info.total = haizei_test_info.success = 0;                 //清空数据
+        func_arr[i].func();                                                    //调用一组测试用例
+        double rate_succ = 100.0 * haizei_test_info.success / haizei_test_info.total;
+        printf(GREEN("[  "));
+
+        if (fabs(rate_succ - 100.0) < 1e-6) {
+            printf(BLUE_HL("%6.2lf%%"), rate_succ);
+        } else {
+            printf(RED_HL("%6.2lf%%"), rate_succ);
+        }
+        printf(
+            GREEN("  ]")  " success : %d    total : %d"  "\n\n",
+            haizei_test_info.success,
+            haizei_test_info.total
+        );
     }
     return 0;
 }
--- project/main.cpp
+++ project/main.cpp
@@ -18,21 +18,18 @@ int add(int a, int b) {
 
 TEST(testFunc, add) {
     EXPECT_EQ(add(5, 3), 8);
-    EXPECT_EQ(add(5, 3), 9);
-    EXPECT_EQ(add(6, 7), 9);
+    EXPECT_NE(add(5, 3), 9);
+    EXPECT_GT(add(6, 7), 10);
+    EXPECT_LE(add(6, 7), 13);
+    EXPECT_GT(add(6, 7), 20);
 }
 
 TEST(testFunc, add2) {
     EXPECT_EQ(add(5, 3), 8);
-    EXPECT_EQ(add(5, 3), 9);
-    EXPECT_EQ(add(6, 7), 14);
-}
-
-TEST(test, Funcadd) {
-    EXPECT_EQ(add(5, 3), 8);
-    EXPECT_EQ(add(5, 3), 9);
+    EXPECT_EQ(add(5, 4), 9);
     EXPECT_EQ(add(6, 7), 13);
 }
+
 
 int main(int argc, char *argv[]) {
     return RUN_ALL_TESTS();
