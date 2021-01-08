6b622becee9c17f73c1fc1b6855f3e33f268f9f6
chenzheyu <2287441467@qq.com>
2021-01-08 12:09:59 +0800

    测试框架初版

--- project/_main.cpp
+++ project/_main.cpp
@@ -8,7 +8,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <pthread.h>
-#include <haizei/test.h>
+#include <gtest/gtest.h>
 #include <iostream>
 using namespace std;
 
@@ -22,18 +22,7 @@ TEST(testFunc, add) {
     EXPECT_EQ(add(6, 7), 9);
 }
 
-TEST(testFunc, add2) {
-    EXPECT_EQ(add(5, 3), 8);
-    EXPECT_EQ(add(5, 3), 9);
-    EXPECT_EQ(add(6, 7), 14);
-}
-
-TEST(test, Funcadd) {
-    EXPECT_EQ(add(5, 3), 8);
-    EXPECT_EQ(add(5, 3), 9);
-    EXPECT_EQ(add(6, 7), 13);
-}
-
 int main(int argc, char *argv[]) {
+    testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
 }
