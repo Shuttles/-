bab16eaa9e87de446e81d2e2948af0f1ac4caedb
chenzheyu <2287441467@qq.com>
2021-03-16 14:42:15 +0800

    在尾部输出每组测试用例的成功率

--- project/review_2/haizei/test.cc
+++ project/review_2/haizei/test.cc
@@ -1,15 +1,28 @@
 #include <haizei/test.h>
 #include <string.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
 
 int func_cnt = 0;
 Function func_arr[100];
+struct FunctionInfo haizei_test_info;
 
 int RUN_ALL_TESTS() {
     for (int i = 0; i < func_cnt; i++) {
         printf(GREEN("[====RUN====]") RED_HL(" %s\n"), func_arr[i].str);
+        haizei_test_info.total = haizei_test_info.success = 0;
         func_arr[i].func();
-        printf("Run End.\n");
+        double rate = 100.0 * haizei_test_info.success / haizei_test_info.total;
+        printf(GREEN("[  ") );
+        if (fabs(rate - 100.0) < 1e-6) {
+            printf(BLUE_HL("%6.2lf%%"), rate);
+        } else {
+            printf(RED_HL("%6.2lf%%"), rate);
+        }
+        printf(GREEN("  ] "));
+        printf(YELLOW_HL("success : %d, total : %d\n"), haizei_test_info.success, haizei_test_info.total);
+
         (i != func_cnt - 1) && printf("\n");
     }
     return 0;
--- project/review_2/haizei/test.h
+++ project/review_2/haizei/test.h
@@ -13,13 +13,13 @@
 
 #define GREEN(a) COLOR(a, 32)
 #define RED(a) COLOR(a, 31)
-#define BLUE(a) COLOR(a, 33)
-#define YELLOW(a) COLOR(a, 34)
+#define YELLOW(a) COLOR(a, 33)
+#define BLUE(a) COLOR(a, 34)
 
 #define GREEN_HL(a) COLOR_HL(a, 32)
 #define RED_HL(a) COLOR_HL(a, 31)
-#define BLUE_HL(a) COLOR_HL(a, 33)
-#define YELLOW_HL(a) COLOR_HL(a, 34)
+#define YELLOW_HL(a) COLOR_HL(a, 33)
+#define BLUE_HL(a) COLOR_HL(a, 34)
 
 
 #define TEST(a, b) \
@@ -33,7 +33,11 @@ void a##_haizei_##b()
 
 #define EXPECT(a, b, comp) {\
     printf(GREEN("[-----------] ") #a " " #comp " " #b);\
-    printf(" %s\n", ((a) comp (b)) ? GREEN_HL("TRUE") : RED_HL("FALSE"));\
+    __typeof(a) _a = (a);\
+    __typeof(b) _b = (b);\
+    haizei_test_info.total += 1;\
+    if (_a comp _b) haizei_test_info.success += 1;\
+    printf(" %s\n", ((_a) comp (_b)) ? GREEN_HL("TRUE") : RED_HL("FALSE"));\
 }
 
 #define EXPECT_EQ(a, b) EXPECT(a, b, ==)
@@ -43,6 +47,7 @@ void a##_haizei_##b()
 #define EXPECT_GT(a, b) EXPECT(a, b, >)
 #define EXPECT_GE(a, b) EXPECT(a, b, >=)
 
+
 typedef void (*TestFuncT)();
 
 struct Function {
@@ -50,8 +55,11 @@ struct Function {
     const char *str;
 };
 
+struct FunctionInfo {
+    int total, success;
+};
 
-
+extern struct FunctionInfo haizei_test_info;
 
 int RUN_ALL_TESTS();
 void add_function(TestFuncT, const char *);
